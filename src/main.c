f07eaad15e7b89e535146b09031811220926c088
Steyndroid <steynsandroid@gmail.com>
2025-05-19 11:47:07 +0200

    UART pins 19/20

--- src/main.c
+++ src/main.c
@@ -103,15 +103,17 @@ void app_main(void)
     ESP_LOGI(TAG, "Starting app_main");
 
     // Initialize UART for Arduino communication
+    ESP_LOGI(TAG, "Setting up UART");
     uart_config_t uart_config = {
         .baud_rate = 115200,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE};
-    uart_param_config(UART_NUM_1, &uart_config);
-    uart_set_pin(UART_NUM_1, 17, 18, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE); // TX: GPIO17, RX: GPIO18
-    uart_driver_install(UART_NUM_1, 256, 256, 0, NULL, 0);
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+    ESP_ERROR_CHECK(uart_param_config(UART_NUM_1, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_1, 20, 19, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE)); // TX: GPIO20, RX: GPIO19
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_1, 256, 256, 0, NULL, 0));
 
     display_init();
 
